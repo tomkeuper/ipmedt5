c6e74eb2ceba6c5cb362aaf6317bc25d75c2dcef
Tijn <tijnvanderweijden@gmail.com>
2021-04-04 12:41:15 +0200

    kleine aanpassing seeds

--- resources/views/sensoren.blade.php
+++ resources/views/sensoren.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="{{URL::asset('js/pushmelding.js')}}" type="text/javascript"></script>
     <title>Document</title>
 </head>
 <body>
     <button><a href="/afstandsensor">Afstandsensor aan/uit</a></button>
 </body>
+
 </html>
