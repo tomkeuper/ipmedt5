93de00ded142f8e2277710a9589c0630aca247a7
Tijn <tijnvanderweijden@gmail.com>
2021-04-04 12:41:57 +0200

    code voor pushnotificaties wip

---
+++ public/js/pushmelding.js
@@ -0,0 +1,34 @@
+function showNotification() {
+    if (!Notification) {
+    $('body').append('<h4 style="color:red">*Browser does not support Web Notification</h4>');
+    return;
+    }
+    $.ajax({
+        url : "notification.php",
+        type: "POST",
+        success: function(data, textStatus, jqXHR) {
+            var data = jQuery.parseJSON(data);
+            if(data.result == true) {
+                var data_notif = data.notif;
+                for (var i = data_notif.length - 1; i >= 0; i--) {
+                    var theurl = data_notif[i]['url'];
+                    var notifikasi = new Notification(data_notif[i]['title'], {
+                        icon: data_notif[i]['icon'],
+                        body: data_notif[i]['msg'],
+                    });
+                    notifikasi.onclick = function () {
+                        window.open(theurl); 
+                        notifikasi.close();     
+                    };
+                    setTimeout(function(){
+                        notifikasi.close();
+                    }, 5000);
+                };
+            } else {
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown)	{}
+    });
+}
+
+console.log("pushmelding");
