4f750b213c9a10263000bcb50683d216aef21c16
dkalemli <dogukankalemli@hotmail.com>
2021-04-06 21:44:07 +0200

    updated view for 'huizen'

--- resources/views/huizen/components/huisCard--index.blade.php
+++ resources/views/huizen/components/huisCard--index.blade.php
@@ -1,10 +1,10 @@
 <li class="a-popup u-list-style-none gridCard">
     <article>
         <header class="gridCard__header u-flex-v-center">
-            <h2 class="gridCard__heading">{{$huis->naam}}</h2>
+            <h2 class="gridCard__heading">{{$huis->name}}</h2>
         </header>
         <section class="gridCard__textSection u-flex-v-center">
-            <p class="gridCard__text">{{$huis->straat}} {{$huis->huisnummer}}</p>
+            <p class="gridCard__text">{{$huis->street}} {{$huis->number}}</p>
         </section>
     </article>
 </li>
