c22ee49010016d21e34cde7e07f64065ca7466d2
dkalemli <dogukankalemli@hotmail.com>
2021-04-07 17:09:36 +0200

    updated view for 'huizen'

--- resources/views/huizen/components/huisCard--index.blade.php
+++ resources/views/huizen/components/huisCard--index.blade.php
@@ -1,14 +1,14 @@
 <li class="a-popup u-list-style-none gridCard">
     <a href="/huizen/{{$huis->id}}">
         <article>
-            <header class="gridCard__header u-flex-h-center u-flex-v-center">
-                <h2 class="gridCard__heading">{{$huis->name}}</h2>
-            </header>
             <figure class="gridCard__figure">
                 <img class="gridCard__image" src="{{$huis->image}}" alt="{{$huis->name}}">
             </figure>
-            <section class="gridCard__textSection u-flex-v-center">
-                <p class="gridCard__text">{{$huis->street}} {{$huis->number}}, {{$huis->postal_code}}</p>
+            <header class="gridCard__header u-flex-h-center u-flex-v-center">
+                <h2 class="gridCard__heading">{{$huis->name}}</h2>
+            </header>
+            <section class="gridCard__textSection u-flex-h-center u-flex-v-center">
+                <p class="gridCard__text">{{$huis->street}} {{$huis->number}}, {{$huis->postal_code}} {{$huis->place}}</p>
             </section>
         </article>
     </a>
