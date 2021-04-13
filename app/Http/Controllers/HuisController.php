f54eb2eed3d838d5a5f66074886069a08b0a7d71
dkalemli <dogukankalemli@hotmail.com>
2021-04-13 17:35:33 +0200

    added controller for sensors

--- app/Http/Controllers/HuisController.php
+++ app/Http/Controllers/HuisController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Huis;
+use App\Models\Sensor;
+use App\Models\Temperatuur;
+use App\Models\Vochtigheid;
 use Auth;
 
 class HuisController extends Controller
@@ -12,13 +15,18 @@ class HuisController extends Controller
         $user = Auth::user()->id;
         return view('dashboard',[
             'huis' => Huis::all()->where('mantelzorger', '==', $user),
-            'user' => Auth::user()
+            'user' => Auth::user(),
         ]);
     }
 
     public function show($id) {
         return view('huizen.show',[
-            'huis' => Huis::find($id)
+            'huis' => Huis::find($id),
+            'paniek' => Sensor::all()->where('huis_id', '==', $id)->where('sensor_id', '==', '1'),
+            'water' => Sensor::all()->where('huis_id', '==', $id)->where('sensor_id', '==', '2'),
+            'gas' => Sensor::all()->where('huis_id', '==', $id)->where('sensor_id', '==', '3'),
+            'temperatuur' => Temperatuur::all()->where('huis_id', '==', $id),
+            'vochtigheid' => Vochtigheid::all()->where('huis_id', '==', $id),
         ]);
     }
 }
