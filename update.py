6dc095d221a5f20e86e92010554bb6ce057c7bb1
Tijn <tijnvanderweijden@gmail.com>
2021-04-15 17:13:48 +0200

    python files toegevoegd

---
+++ update.py
@@ -0,0 +1,14 @@
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="127.0.0.1",
+    user="laravel",
+    passwd="laravel",
+    database="mantelzorg"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("UPDATE beveiliging SET triggered = true;")
+mydb.commit()
