9d0146c402ecc4661bb6af5b807839789e71d8e4
dkalemli <dogukankalemli@hotmail.com>
2021-04-16 20:33:30 +0200

    added logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <img src="/img/logo/mantelzorger.png" alt="Mantelzorgers." width="85">
+                        <!-- <x-application-logo class="block h-10 w-auto fill-current text-gray-600" /> -->
                     </a>
                 </div>
 
